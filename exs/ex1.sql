29cbdbaa5a87e1f8e237093a45c2507817f0b87a
cauerast <cauerast@gmail.com>
2026-05-07 15:59:36 -0300

    lec1 update

--- exs/ex1.sql
+++ exs/ex1.sql
@@ -1,6 +1,6 @@
--- Active: 1778113673688@@127.0.0.1@3306
+-- Active: 1778165136825@@127.0.0.1@3306
 
-CREATE TABLE Aluno(
+CREATE TABLE Alunos(
   RA int,
   nome varchar(80),
   fone varchar(30),
@@ -8,3 +8,7 @@ CREATE TABLE Aluno(
   mae varchar(80),
   data_nasc date
 );
+INSERT INTO Alunos
+VALUES (123, 'MATEUS', '11990000000', 'Daniel', 'Clara', 2002-05-06),
+        (124, 'MARCOS', '169923443243', 'Fernando', 'Luisa'),
+        (125, 'LUCAS', '5498237746', 'Luis', '');
