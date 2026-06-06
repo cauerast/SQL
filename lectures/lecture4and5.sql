574f3ed54c13e78e979a0797f2166e1dae703cbe
cauerast <cauerast@gmail.com>
2026-06-06 14:21:39 -0300

    lecture 5 exs finshed

--- lectures/lecture4and5.sql
+++ lectures/lecture4and5.sql
@@ -46,8 +46,6 @@ values ('RODOLFO', 1330);
 
 
 
-select * from especialidades;
-select * from medicos;
 
 -- joins
 -- medicos com especialidades e sem especialidades
@@ -76,7 +74,7 @@ WHERE e.codEsp IS NULL;
 create table pacientes (
     codPac int PRIMARY KEY IDENTITY(1,1),
     nome varchar(80),
-    fone varchat(30)
+    fone varchar(30)
 )
 
 -- create table consulta
@@ -92,28 +90,133 @@ create table consultas (
 
 -- joins
 SELECT m.nome AS nomeMedico, c.data, p.nome AS nomePaciente FROM 
-    medico AS m INNER JOIN consulta AS c
+    medicos AS m INNER JOIN consultas AS c
         ON m.codMed = c.codMed
-    INNER JOIN paciente AS p
+    INNER JOIN pacientes AS p
         ON p.codPac = c.codPac
     INNER JOIN especialidades as e
         ON e.codEsp = m.codEsp
 WHERE c.data >= '2026/05/20' AND c.data <= '2026/05/31' 
     AND e.nome = 'PEDIATRA';
 
+
+
+select * from especialidades;
+select * from medicos;
+select * from consultas;
+select * from pacientes;
+
+
 -- Fix exercises
 -- 1) Cadastre 6 pacientes
+INSERT INTO pacientes
+VALUES ('pac1', '1199123-4567'),
+       ('pac2', '1199123-4567'),
+       ('pac3', '1199123-4567'),
+       ('pac4', '1199123-4567'),
+       ('pac5', '1199123-4567'),
+       ('pac6', '1199123-4567');
+
+
 -- 2) Cadastre 10 consultas para medicos e pacientes diversos
--- 3) Atualize o nome do médico 'JOAO' para 'JOÃO DA SILVA'
+INSERT INTO consultas
+VALUES ('2026/10/1', 'SUS', 1, 1),
+       ('2026/1/2', 'UNIMED', 2, 1),
+       ('2026/10/3', 'SUS', 3, 2),
+       ('2026/7/4', 'UNIMED', 4, 2),
+       ('2026/10/25', 'UNIMED', 5, 3),
+       ('2026/10/2', 'UNIMED', 6, 3),
+       ('2026/2/21', 'SUS', 7, 4),
+       ('2026/8/8', 'SUS', 8, 4),
+       ('2026/6/18', 'UNIMED', 9, 5),
+       ('2026/8/1', 'SUS', 10, 6)
+
+-- 3) Atualize o nome do medico 'JOAO' para 'JOÃO DA SILVA'
+UPDATE medicos 
+SET nome = 'JOÃO DA SILVA'
+WHERE NOME = 'JOAO';
+
 -- 4) A data da consulta numero 3 é 15/maio/2025 (atualize essa info)
+UPDATE consultas 
+SET data = '2025/5/15'
+WHERE codCon = 3;
+
 -- 5) Exclua a primeira consulta cadastrada
+DELETE FROM consultas 
+WHERE codCon = 1;
+
 -- 6) Liste os nomes dos médicos e a especialidade de cada um
+SELECT m.nome as nomeMed, e.nome as nomeEsp
+FROM medicos as m LEFT JOIN especialidades as e
+ON m.codEsp = e.codEsp
+
 -- 7) Liste os médicos que nao têm especialidades
+SELECT m.nome as nomeMedico
+FROM medicos as m LEFT JOIN especialidades as e
+ON m.codEsp = e.codEsp
+WHERE e.codEsp IS NULL;
+
 -- 8) liste as consultas feitas pelo convenio UNIMED no mes de abril
+SELECT * 
+FROM consultas
+WHERE convenio = 'UNIMED'
+
 -- 9) liste os nomes dos pacientes e os convenios que usaram nas suas consultas
+select p.nome as nomePaciente, c.convenio as convenioPaciente
+from pacientes as p LEFT JOIN consultas as c 
+ON p.codPac = c.codPac;
+
+
 -- 10) Liste os telefones dos pacientes que nunca consultaram
+SELECT p.fone 
+FROM pacientes AS p 
+LEFT JOIN consultas AS c 
+    ON p.codPac = c.codPac 
+WHERE c.codCon IS NULL;
+
 -- 11) Liste os convênios das consultas feitas por ORTOPEDISTAS
+select c.convenio
+from consultas as c INNER JOIN medicos as m
+on c.codMed = m.codMed
+INNER JOIN especialidades as e
+on e.codEsp = m.codEsp
+where e.nome = 'ORTOPEDISTA'
+
 -- 12) Liste os nomes e fones dos pacientes atendidos por PEDIATRAS ou DERMATOLOGISTAS em abril/2026
+select p.nome, p.fone 
+from pacientes as p INNER JOIN consultas as c
+    on p.codPac = c.codPac
+INNER JOIN medicos as m
+    on m.codMed = c.codMed
+INNER JOIN especialidades as e
+    on e.codEsp = m.codEsp
+WHERE (e.nome = 'PEDIATRA' or e.nome = 'DERMATOLOGISTA') and c.data BETWEEN '2026/04/01' and '2026/04/30';
+
 -- 13) Cadastre a especialidade 'NEUROLOGISTA' e atualize as especialidades de 2 médicos para esta nova
+insert into especialidades
+values ('NEUROLOGISTA');
+
+update medicos
+set codEsp = 70
+where codMed = 1 or codMed = 2 or codEsp IS NULL;
+
 -- 14) Crie 3 consultas para médicos NEUROLOGISTAS no mes de maio/2026
--- 15) As consultas feitas pelos PEDIATRAS em abril/2026 devem se somente do convenio 'SUS'. Atualize essa info
+insert into consultas
+values ('2026/10/1', 'UNIMED', 11, 4),
+       ('2026/06/1', 'SUS', 12, 5),
+       ('2026/04/1', 'UNIMED', 7, 6)
+
+-- 15) As consultas feitas pelos PEDIATRAS em abril/2026 devem ser somente do convenio 'SUS'. Atualize essa info.
+update c -- consultas, mas como eu renomeei ali em baixo seria melhor usar aqui tambem o c' como consultas'
+set convenio = 'SUS'
+from especialidades as e INNER JOIN medicos as m
+    on e.codEsp = m.codEsp
+INNER JOIN consultas as c
+    on c.codMed = m.codMed
+WHERE e.nome = 'PEDIATRA' and c.data BETWEEN '2026/04/01' AND '2026/04/30';
+
+
+select * from especialidades;
+select * from medicos;
+select * from consultas;
+select * from pacientes;
